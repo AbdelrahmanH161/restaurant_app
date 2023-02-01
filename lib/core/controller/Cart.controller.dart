e49cc91d7337a538a486c47171592fdd6bd49cbe
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2023-02-01 21:03:58 +0200

    updata

--- lib/core/controller/Cart.controller.dart
+++ lib/core/controller/Cart.controller.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:restaurant_app/core/models/Addition.model.dart';
 import 'package:restaurant_app/core/models/CartModal.dart';
 
